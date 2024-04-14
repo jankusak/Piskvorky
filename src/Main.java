86bddd5c737cbba31e43ae45e3f853272cbea211
Jan Valasek <jan.valasek89@seznam.cz>
2024-04-14 21:27:49 +0200

    Language improved

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,18 @@ import java.util.ArrayList;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private enum Hrac {
+    private enum Player {
         X, O
     }
 
     private static String[][] playField = new String[3][3];
-    private static Hrac currentHrac = Hrac.O;
-
+    private static Player currentPlayer = Player.X;
     private static ArrayList<Integer> chosenNumbers = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("Ahoj, vitam te ve hre piskvorky.");
+        System.out.println("Hi, welcome to the Tic-tac-toe game");
 
         Scanner scanner = new Scanner(System.in);
-
 
         for (int row = 0; row < 3; row++) {
             for (int column = 0; column < 3; column++) {
@@ -26,10 +24,8 @@ public class Main {
             }
         }
         while (true) {
-
-            System.out.println("Hrac " + currentHrac.name() + ": Zadej 1 - 9");
+            System.out.println("Player " + currentPlayer.name() + ": Chose number 1 - 9");
             String gamer = scanner.nextLine();
-
 
             try {
                 Integer numberGamer = Integer.valueOf(gamer);
@@ -37,27 +33,21 @@ public class Main {
                     if (chosenNumbers.contains(numberGamer)) {
                         System.out.println("Number is already chosen");
                     } else {
-                        setupTable(numberGamer);
-
+                        applyPosition(numberGamer);
                         chosenNumbers.add(numberGamer);
-                        if (testResult(currentHrac.name())) {
+                        if (testResult(currentPlayer.name())) {
                             return;
                         }
                         printTable();
-                        currentHrac = currentHrac == Hrac.O ? Hrac.X : Hrac.O;
+                        currentPlayer = currentPlayer == Player.O ? Player.X : Player.O;
                     }
-
                 } else {
-
                     System.out.println("Invalid number!!! Try number between 1 - 9");
                 }
-
             } catch (Exception e) {
                 System.out.println("Invalid  input. Try it again.");
             }
-
         }
-
     }
 
     private static void printTable() {
@@ -67,77 +57,60 @@ public class Main {
         System.out.println(playField[1][0] + "|" + playField[1][1] + "|" + playField[1][2]);
         System.out.println("-----");
         System.out.println(playField[2][0] + "|" + playField[2][1] + "|" + playField[2][2]);
-
     }
 
-    private static void setupTable(int fieldNumber) {
-
-
+    private static void applyPosition(int fieldNumber) {
         switch (fieldNumber) {
-            case 1 -> playField[0][0] = (currentHrac == Hrac.O ? Hrac.O.name() : Hrac.X.name());
-            case 2 -> playField[0][1] = (currentHrac == Hrac.O ? Hrac.O.name() : Hrac.X.name());
-            case 3 -> playField[0][2] = (currentHrac == Hrac.O ? Hrac.O.name() : Hrac.X.name());
-            case 4 -> playField[1][0] = (currentHrac == Hrac.O ? Hrac.O.name() : Hrac.X.name());
-            case 5 -> playField[1][1] = (currentHrac == Hrac.O ? Hrac.O.name() : Hrac.X.name());
-            case 6 -> playField[1][2] = (currentHrac == Hrac.O ? Hrac.O.name() : Hrac.X.name());
-            case 7 -> playField[2][0] = (currentHrac == Hrac.O ? Hrac.O.name() : Hrac.X.name());
-            case 8 -> playField[2][1] = (currentHrac == Hrac.O ? Hrac.O.name() : Hrac.X.name());
-            case 9 -> playField[2][2] = (currentHrac == Hrac.O ? Hrac.O.name() : Hrac.X.name());
+            case 1 -> playField[0][0] = (currentPlayer == Player.O ? Player.O.name() : Player.X.name());
+            case 2 -> playField[0][1] = (currentPlayer == Player.O ? Player.O.name() : Player.X.name());
+            case 3 -> playField[0][2] = (currentPlayer == Player.O ? Player.O.name() : Player.X.name());
+            case 4 -> playField[1][0] = (currentPlayer == Player.O ? Player.O.name() : Player.X.name());
+            case 5 -> playField[1][1] = (currentPlayer == Player.O ? Player.O.name() : Player.X.name());
+            case 6 -> playField[1][2] = (currentPlayer == Player.O ? Player.O.name() : Player.X.name());
+            case 7 -> playField[2][0] = (currentPlayer == Player.O ? Player.O.name() : Player.X.name());
+            case 8 -> playField[2][1] = (currentPlayer == Player.O ? Player.O.name() : Player.X.name());
+            case 9 -> playField[2][2] = (currentPlayer == Player.O ? Player.O.name() : Player.X.name());
             default -> System.out.println("Something wrong");
         }
     }
 
     public static boolean testResult(String gamerSymbol) {
-
-
-
-
         for (int row = 0; row < 3; row++) {
             for (int column = 0; column < 3; column++) {
-
-
-                try{
-                    if (playField[row][column] != gamerSymbol){
+                try {
+                    if (playField[row][column] != gamerSymbol) {
                         continue;
                     }
-
-////                    tohle je cunarna, ale zatim me nenapadlo, jak ten algoritmus opravit nebo vymyslet lepsi
+//                    tohle je cunarna, ale zatim me nenapadlo, jak ten algoritmus opravit nebo vymyslet lepsi
                     if (playField[0][0].equals(playField[1][0]) && playField[1][0].equals(playField[2][0])) {
-                        System.out.println("Gamer " + currentHrac.name() + " win");
+                        System.out.println("Gamer " + currentPlayer.name() + " win");
                         return true;
                     }
 //                    horizontalni vyhra
                     if (playField[row][column].equals(playField[row][column - 1]) && playField[row][column].equals(playField[row][column + 1])) {
-                        System.out.println("Gamer " + currentHrac.name() +" win");
+                        System.out.println("Gamer " + currentPlayer.name() + " win");
                         return true;
-
                     }
 //                  vertikalni vyhra
                     if (playField[row][column].equals(playField[row + 1][column]) && playField[row][column].equals(playField[row + 1][column])) {
-                        System.out.println("Gamer " + currentHrac.name() +" win");
+                        System.out.println("Gamer " + currentPlayer.name() + " win");
                         return true;
-
                     }
-
                     //           diagonala
                     if (row == 1 && column == 1) {
                         if (playField[row][column].equals(playField[row - 1][column - 1]) && playField[row][column].equals(playField[row + 1][column + 1])) {
-                            System.out.println("Gamer " + currentHrac.name() +" win");
+                            System.out.println("Gamer " + currentPlayer.name() + " win");
                             return true;
-
                         }
                         if (playField[row][column].equals(playField[row + 1][column - 1]) && playField[row][column].equals(playField[row - 1][column + 1])) {
-                            System.out.println("Gamer " + currentHrac.name() +" win");
+                            System.out.println("Gamer " + currentPlayer.name() + " win");
                             return true;
-
                         }
-
                     }
                 } catch (Exception ArrayIndexOutOfBoundsException) {
-                //Ted nevadi, ze jsou hodnoty mimo rozsah
+                    //Ted nevadi, ze jsou hodnoty mimo rozsah
                 }
             }
-
         }
         if (chosenNumbers.size() >= 9) {
             System.out.println("The match ended in a tie");
@@ -145,6 +118,5 @@ public class Main {
         }
         return false;
     }
-
 }
 
